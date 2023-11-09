7b1390f031c7bbd1828fc9c8d1ff9e957033141a
Kate <ketkatarina14@gmail.com>
2023-11-09 16:17:30 +0200

    Add adapting for carousel

--- src/js/script.js
+++ src/js/script.js
@@ -34,31 +34,25 @@ $(document).ready(function(){
 }); */
 
 const slider = tns({
-    container: '.carousel__inner',
-    items: 1,
-    slideBy: 'page',
-    autoplay: false,
-    controls: false,
-    nav: false,
-    responsive: {
-      640: {
-        edgePadding: 20,
-        gutter: 20,
-        items: 1
-      },
-      700: {
-        gutter: 30
-      },
-      900: {
-        items: 1
-      }
+  container: '.carousel__inner',
+  items: 1,
+  slideBy: 'page',
+  autoplay: false,
+  controls: false,
+  nav: true,
+  speed: 1200,
+  responsive: {
+    1200: {
+      nav: false,
+    },
+    992: {
+      nav: true
     }
+  }
 });
 document.querySelector('.prev').addEventListener('click', function () {
-  slider.goTo('prev');
+  slider.goTo('prev')
 });
 document.querySelector('.next').addEventListener('click', function () {
-  slider.goTo('next');
+  slider.goTo('next')
 });
-
-    
